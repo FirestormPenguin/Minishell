49aa158a1b3fc4abd327b79b9766ec2f2cbb9f2f
egiubell <121073200+FirestormPenguin@users.noreply.github.com>
2023-11-04 01:21:52 +0000

    cose

--- minishell.c
+++ minishell.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
+//gcc -o minishell minishell.c -lreadline
+
+int main() {
+    char *inputString;
+
+    while (1) {
+        inputString = readline("");
+        if (strcmp(inputString, "exit") == 0)
+        {
+            free(inputString);
+            break;
+        }
+        printf("Hai inserito: %s\n", inputString);
+        add_history(inputString);
+        free(inputString);
+    }
+    return 0;
+}
