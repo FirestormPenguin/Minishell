cbb936be254a9757e5ae90416eac8a7cf22448fe
egiubell <121073200+FirestormPenguin@users.noreply.github.com>
2023-11-09 17:34:02 +0000

    commenti

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:54:10 by codespace         #+#    #+#             */
-/*   Updated: 2023/11/09 15:45:43 by codespace        ###   ########.fr       */
+/*   Updated: 2023/11/09 17:33:08 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,26 @@ void exe(char *str)
     char    path[50] = "/bin/";
     char    *args[] = {str, NULL};
     
+    /*con strcat unisco al path il comando
+    NOTA: non si puo' usare strcat, da mettere ft_strcat*/
     strcat(path, str);
+    /*forka il processo creando un figlio*/
     pid = fork();
-    if (pid)            //if is in pid, siamo nel processo padre
+    /*if is in pid, siamo nel processo padre
+    che tramite wait attende che i figli si chiudano,
+    else siamo nel processo figlio, i due processi
+    lavorano in contemporanea*/
+    if (pid)
     {
-        printf("%d\n", pid);
-        waitpid(-1, &status, WUNTRACED);    //wait qualunque figlio esistente
-        printf("%d\n", status);
+        //printf("%d\n", pid);
+        waitpid(-1, &status, WUNTRACED);
+        //printf("%d\n", status);
     }
-    else                //else siamo nel processo figlio
+    else
     {
+        /*execve esegue un comando di bash tramite path (/bin/...)
+        e comando di per se (ls, pwd, etc), args normalmente
+        contiene il comando + flag (opzionale) + NULL*/
         execve(path, args, NULL);
         exit(0);
     }
@@ -41,17 +51,21 @@ void getInput()
     
     while (1)
     {
-        inputString = readline("<Minishell> ");          //utilizzo readline per prendere da terminale l'input
-        if (!inputString)                               //if inputString is NULL (cntrl + D) break
+        /*utilizzo readline per prendere da terminale l'input*/
+        inputString = readline("<Minishell> ");      
+        /*if inputString is NULL (cntrl + D) break*/    
+        if (!inputString)
             break;
-        if (strcmp(inputString, "exit") == 0)           //if inputString is "exit", break
+        /*if inputString is "exit", break*/
+        if (strcmp(inputString, "exit") == 0)
         {
             free(inputString);
             break;
         }
         else
             exe(inputString);
-        add_history(inputString);                       //aggiunge alla history il comando appena scritto
+        /*aggiunge alla history il comando appena scritto*/
+        add_history(inputString);
         free(inputString);
     }
 }
@@ -61,6 +75,7 @@ int main(int ac, char **av, char **envp)
     (void) ac;
     (void) av;
 
-    getInput();         //prende l'input da terminale
+    /*prende l'input da terminale*/
+    getInput();
     return (0);
 }
