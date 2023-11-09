9bda26dcfca8eadf0598914d937f21cf24d92bcd
egiubell <121073200+FirestormPenguin@users.noreply.github.com>
2023-11-09 15:46:16 +0000

    aggunto func exe

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:54:05 by codespace         #+#    #+#             */
-/*   Updated: 2023/11/09 13:54:25 by codespace        ###   ########.fr       */
+/*   Updated: 2023/11/09 13:57:10 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,3 @@
 #include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-
--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,34 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:54:10 by codespace         #+#    #+#             */
-/*   Updated: 2023/11/09 13:54:12 by codespace        ###   ########.fr       */
+/*   Updated: 2023/11/09 15:45:43 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <../include/minishell.h>
+
+void exe(char *str)
+{
+    pid_t   pid;
+    int     status;
+    char    path[50] = "/bin/";
+    char    *args[] = {str, NULL};
+    
+    strcat(path, str);
+    pid = fork();
+    if (pid)            //if is in pid, siamo nel processo padre
+    {
+        printf("%d\n", pid);
+        waitpid(-1, &status, WUNTRACED);    //wait qualunque figlio esistente
+        printf("%d\n", status);
+    }
+    else                //else siamo nel processo figlio
+    {
+        execve(path, args, NULL);
+        exit(0);
+    }
+    
+}
 
 void getInput()
 {
@@ -18,7 +41,7 @@ void getInput()
     
     while (1)
     {
-        inputString = readline("Minishell: ");          //utilizzo readline per prendere da terminale l'input
+        inputString = readline("<Minishell> ");          //utilizzo readline per prendere da terminale l'input
         if (!inputString)                               //if inputString is NULL (cntrl + D) break
             break;
         if (strcmp(inputString, "exit") == 0)           //if inputString is "exit", break
@@ -26,7 +49,8 @@ void getInput()
             free(inputString);
             break;
         }
-        printf("Hai inserito: %s\n", inputString);
+        else
+            exe(inputString);
         add_history(inputString);                       //aggiunge alla history il comando appena scritto
         free(inputString);
     }
@@ -36,20 +60,7 @@ int main(int ac, char **av, char **envp)
 {
     (void) ac;
     (void) av;
-    pid_t pid;
-    int status;
 
     getInput();         //prende l'input da terminale
-    pid = fork();       //crea un processo figlio in cui manderemo l'exec
-    if (pid)            //if is in pid, siamo nel processo padre
-    {
-        printf("%d\n", pid);
-        waitpid(-1, &status, WUNTRACED);    //wait qualunque figlio esistente
-        printf("%d\n", status);
-    }
-    else                //else siamo nel processo figlio
-    {
-        exit(0);
-    }
     return (0);
 }
