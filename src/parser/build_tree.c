24a22dcd641294937423629d58670d4f6800e2ae
FirestormPenguin <edoardogiubellini@gmail.com>
2024-01-19 16:59:46 +0100

    tentativi per le pipe e commenti aggiunti per le pipe che non funzionano

--- src/parser/build_tree.c
+++ src/parser/build_tree.c
@@ -6,7 +6,7 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:54:10 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/19 16:27:30 by egiubell         ###   ########.fr       */
+/*   Updated: 2024/01/19 16:59:16 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,14 +40,20 @@ t_tree	*insert_pipe(t_args *tokens, int pipe_count)
 	return (rootNode);
 }
 
-void assignTmp(t_tree *tmp, t_tree *rootNode)
+void assignNotWord(t_tree *newNode, t_tree *rootNode, int deleted_pipe)
 {
-	if (rootNode)
+	if (rootNode && rootNode->type != TOKEN_PIPE)
 	{
-		if (rootNode->right != NULL)
-			assignTmp(tmp, rootNode->right);
+		if (rootNode->right)
+			assignNotWord(newNode, rootNode->right, deleted_pipe);
 		else
-			rootNode->right = tmp;
+			rootNode->right = newNode;
+	}
+	/*non funge bene, da sistemare in generale*/
+	else if (rootNode && rootNode->type == TOKEN_PIPE && deleted_pipe == 0)
+	{
+		if (rootNode->left)
+			assignNotWord(newNode, rootNode->left, deleted_pipe);
 	}
 }
 
@@ -60,17 +66,17 @@ t_tree  *parseTokens(t_args *tokens, t_tree *prevNode, t_tree *rootNode, int tok
 		return (NULL);
 	newNode = create_node(tokens->str, tokens->type, prevNode, rootNode);
 	if (newNode->type != TOKEN_WORD)
-		assignTmp(newNode, rootNode);
+		assignNotWord(newNode, rootNode, deleted_pipe);
 	tokens++;
 	token_count--;
-	// if (tokens->type == TOKEN_PIPE && deleted_pipe == 0)
-	// {
-	// 	/*questa parte funziona ma bisogna far si che non solo salti la pipe ma anche
-	// 	che faccia tornare l'esecuzione come fosse un normale token non word*/
-	// 	tokens++;
-	// 	token_count--;
-	// 	deleted_pipe = 1;
-	// }
+		/*questa parte funziona ma bisogna far si che non solo salti la pipe ma anche
+		che faccia tornare l'esecuzione come fosse un normale token non word*/
+	if (tokens->type == TOKEN_PIPE && deleted_pipe == 0)
+	{
+		tokens++;
+		token_count--;
+		deleted_pipe = 1;
+	}
 	if (tokens->type == TOKEN_WORD)
 		newNode->left = parseTokens(tokens, newNode, rootNode, token_count, deleted_pipe);
 	else
@@ -81,7 +87,6 @@ t_tree  *parseTokens(t_args *tokens, t_tree *prevNode, t_tree *rootNode, int tok
 			tmp = tmp->right;
 		}
 		tmp->right = parseTokens(tokens, tmp, rootNode, token_count, deleted_pipe);
-		// assignTmp(tmp, rootNode);
 	}
 	return (newNode);
 }
