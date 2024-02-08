e8f398697f68aeae5bb66ac3983bfa8809179df0
MirkokriM <mirkovenditti33@gmail.com>
2024-02-09 00:21:47 +0100

    qsort spiegato da cristo, export gestire ulteriori casi d'errore ma sia il sort sia il set funziona già bene

--- libft/src/ft_qsort.c
+++ libft/src/ft_qsort.c
@@ -53,3 +53,21 @@ void    ft_qsort(char **arr, int low, int high)
         ft_qsort(arr, pi + 1, high);
     }
 }
+
+/* Dopo la chiamata iniziale, l'array viene partizionato attorno al pivot "a" (l'ultimo elemento)
+Tutti gli elementi minori di "a" vengono spostati a sinistra, e tutti gli elementi maggiori a destra
+Ma dato che "a" è l'elemento più piccolo, finisce per essere il primo elemento dell'array
+Array dopo la partizione: {"a", "e", "d", "c", "b", "f"}
+
+Ora ft_qsort viene chiamato ricorsivamente per i sottosegmenti a sinistra e a destra del pivot
+
+Sottosegmento sinistro: {"e", "d", "c", "b"}
+Il pivot per questo sottosegmento è "b"
+Dopo la partizione, l'array diventa: {"a", "b", "d", "c", "e", "f"}
+
+Sottosegmento sinistro del sottosegmento sinistro: {"d", "c"}
+Il pivot per questo sottosegmento è "c"
+Dopo la partizione, l'array diventa: {"a", "b", "c", "d", "e", "f"}
+
+A questo punto, tutti i sottosegmenti sinistri sono ordinati
+Non ci sono sottosegmenti destri in questo esempio, quindi l'array è completamente ordinato */
